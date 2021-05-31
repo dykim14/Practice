28ae6c8eed5794ea8ab42f07901d0b9d15e05d6e
dykim14 <dykim14@gmail.com>
2021-05-31 18:33:12 +0900

    Java 객체지향 디자인 패턴 - Chapter 01 객체지향 모델링

---
+++ JavaOODesignPattern/src/com/practice/chapter1/Course.java
@@ -0,0 +1,15 @@
+package com.practice.chapter1;
+
+public class Course {
+    private String id;
+    private String name;
+    private Integer numOfStudents = 0;
+
+    public void addStudent() {
+
+    }
+
+    public void deleteStudent() {
+
+    }
+}
